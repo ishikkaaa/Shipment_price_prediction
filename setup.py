e02c3e1387b8a14c7708084234f6b35b08a628a3
Ishika Aggarwal <ishikaaggarwal749@gmail.com>
2025-04-29 13:32:30 +0530

    added readme file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+  name="shipment",
+  version="0.0.1",
+  author="Ishika",
+  author_email="ishikaaggarwal749@gmail.com",
+  packages=find_packages(),
+  install_requires=[]
+)
